8d675263a6fcb71c13024a6e7d1b6042f210c2ae
unknown <carlimar2002@gmail.com>
2025-03-29 11:28:01 -0400

    movie_detail

--- movies/urls.py
+++ movies/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 
     path('', MovieListView.as_view(), name='movie_list'),      #Aqui se registra la url, si se colocan mas url se coloca una coma","
     path('movie/detail/<int:pk>', MovieDetailView.as_view(), name='movie_details')
+    # int:pk es el id de la pelicula, se coloca pk porque es el nombre del campo en la base de datos
+    # en la url se colocaria en id de la base de datos (movie/detail/1) y se mostraria la pelicula con id 1
 
 ]
