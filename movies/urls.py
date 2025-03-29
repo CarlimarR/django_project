303e77d6aec4e581ad8795c29b953abc53aba1cd
unknown <carlimar2002@gmail.com>
2025-03-29 09:52:18 -0400

    feture/movie_detail

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import MovieListView
+from.views import MovieDetailView
 
 urlpatterns = [
 
-    path('', MovieListView.as_view(), name='movie_list')           #Aqui se registra la url
+    path('', MovieListView.as_view(), name='movie_list'),      #Aqui se registra la url, si se colocan mas url se coloca una coma","
+    path('movie/detail/<int:pk>', MovieDetailView.as_view(), name='movie_details')
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -6,5 +6,11 @@ from movies.models import Movies
 # Create your views here.
 
 class MovieListView(ListView):                      #Aqui se registra la view
+    model = Movies                                   #aqui el modelo es el de la clase que se registre en models.py
+    template_name = 'movies/movie_list.html'         #NO olvides registrar los templates tambien
+
+class MovieDetailView(DetailView):                    #Aqui se registra la view
+
     model = Movies                                   #aqui el modelo es el de la clsse que se registre en models.py
-    template_name = 'movies/movie_list.html'         #NO olvides registrar los templas tambien
+    template_name = 'movies/movie_detail.html'        #NO olvides registrar los templates tambien
+
